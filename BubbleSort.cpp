0733f406024a56b50b5e4ac412d5dd391994486b
honpham <phhon24@gmail.com>
2018-08-01 12:25:18 +0700

    fix at line 14 n to n - 1

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -11,7 +11,7 @@ void swap (int *a, int *b) {
 
 // A function to implement bubble sort
 void bubbleSort(int a[], int n) {
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n-1; i++) {
 		// Variable to check if two elements were swapped by inner loop or not
 		bool checkswap = false;
 		// j < n-i-1 because last element are already in place after each loop
