f593c5b8a39e45152912184bb6e3347fe5802997
honpham <phhon24@gmail.com>
2018-09-26 16:09:10 +0700

    Quick Sort Version 3

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,53 +1,51 @@
-// C++ implementation Quick Sort
-
+// C++ program to implement Quick Sort
 #include <iostream>
 using namespace std;
 
 // A utility function to swap two elements
-void swap(int* a, int* b) {
+void swap (int *a, int *b) {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
 // This function takes last element as pivot, places the pivot element at its correct position in sorted array
-// and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
-
-int partition(int arr[], int low, int high) {
-	int pivot = arr[high]; // pivot element
-	int i = low - 1; // Index of smaller element than pivot
-	for (int j = low; j < high-1; j++) {
-		if (arr[j] <= arr[pivot]) {
-			i++; // Increment index of smaller element
-			swap(arr[i], arr[j]);
+// And places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
+int partition(int arr[], int l, int r) {
+	int i = l; // Index of smaller element
+	int pivot = arr[r]; // pivot value
+	for (int j = l; j < r; j++) {
+		// If current element is smaller than of equal to pivot
+		if (arr[j] <= pivot) {
+			swap(&arr[j], &arr[i]);
+			i++; // increment index of smaller element
 		}
 	}
-	swap (arr[i+1], arr[pivot]);
-	return (i+1);
+	swap(&arr[i], &arr[r]);
+	return i;
 }
+
 // The main function that implements quick sort
 // arr[] --> Array to be sorted
-// low --> Starting index
-// high --> Ending index
+// l --> Starting index
+// r --> Ending index
 void quickSort(int arr[], int l, int r) {
-	if (low < high) {
-		// pi is partitioning index, arr[pi] is now at right place
-		int pi = partition(arr, low, high);
-		// Separately sort elements before partition and after partition
-		quickSort(arr, low, pi-1);
-		quickSort(arr, pi+1, high);
+	if (l < r) {
+		// pivot is partitioning index, arr[pivot] is now at right place
+		int pivot = partition(arr, l, r);
+		// Separately sort elements before and after partition
+		quickSort(arr, l, pivot - 1);
+		quickSort(arr, pivot + 1, r);
 	}
 }
 
+// Driver program to test above functions
 int main() {
-	int arr[] = {10, 80, 30, 90, 40, 50, 70};
-	int n = sizeof(arr)/sizeof(arr[0];
-	quickSort(arr, 0, n-1);
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
 	for (int i = 0; i < n; i++)
-		count << arr[i] << endl;
-	system ("pause");
+		cout << arr[i] << endl;
+	system("pause");
 	return 0;
 }
-	
-	
-			
