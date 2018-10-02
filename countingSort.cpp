b9339a0eeeaee8272e5ef14492561903c864e1f1
honpham <phhon24@gmail.com>
2018-10-02 17:06:50 +0700

    Counting Sort

---
+++ countingSort.cpp
@@ -0,0 +1,49 @@
+/*
+C++ program for Counting Sort
+*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+#define K 255
+
+// The main function that sort the given string arr[] in alphabatical order
+void countingSort(char arr[]) {
+
+	// The output character array that will have sorted arr
+	char aux[strlen(arr)];
+
+	// Create a count array to store count of inidividual
+	// characters and initialize count array as 0
+	int count_arr[K+1];
+	for (int i = 0; i <= K; i++)
+		count_arr[i] = 0;
+	/*
+	memset(count_arr, 0, sizeof(count_arr));
+	*/
+	// Store count of each character
+	for (int i = 0; i < strlen(arr); i++)
+		count_arr[arr[i]] = count_arr[arr[i]] + 1;
+
+	// Change count_arr[i] so that count_arr[i] now contains actual
+	// position of this character in output array
+	for (int i = 1; i <= K; i++)
+		count_arr[i] = count_arr[i-1] + count_arr[i];
+
+	// Build the auxiliary character array
+	for (int i = 0; i < strlen(arr); i++) {
+		aux[count_arr[arr[i]] - 1] = arr[i];
+		count_arr[arr[i]] = count_arr[arr[i]] = 1;
+	}
+	// Copy the auxiliary array to arr, so that arr now contains sorted characters
+	for (int i = 0; i < strlen(arr); i++)
+		arr[i] = aux[i];
+}
+
+// Driver program to test above function
+int main() {
+	char arr[] = "Hon Dep Trai";
+	countingSort(arr);
+	cout << arr << endl;
+	return 0;
+	system("pause");
+}
